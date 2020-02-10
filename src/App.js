97a97b1c1c412773a77e48744fdbd7a812ea4f97
Bhawani Shankar <g8bhawani@gmail.com>
2020-02-10 16:20:30 +0530

    Demo test AWS S3 Deployment test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CircleCI Deployment Done! 
         </p>
         <a
           className="App-link"
